c286639a3f383ab8de1dd68a98c235e8e9d2024c
Quang <quangm202@gmail.com>
2023-07-22 17:38:51 +0700

    Tạo migration cho các bảng trong database

---
+++ database/migrations/2023_07_22_101739_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('first_nam');
+            $table->string('last_name');
+            $table->string('company_name')->nullable();
+            $table->string('country');
+            $table->string('postcode_zip');
+            $table->string('town_city');
+            $table->string('email');
+            $table->string('phone');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
