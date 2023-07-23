75e733bda7ef165856dc8ccfa02f3146e20a01d2
Quang <quangm202@gmail.com>
2023-07-23 08:51:17 +0700

    Hoàn thiện ProductService

--- app/Service/ServiceInterface.php
+++ app/Service/ServiceInterface.php
@@ -4,5 +4,9 @@ namespace App\Service;
 
 interface ServiceInterface
 {
-
+    public function all();
+    public function find($id);
+    public function create(array $data);
+    public function update(array $data, $id);
+    public function delete($id);
 }
