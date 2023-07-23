f162c6705e3cadca9843eb6d6e686027ad4ff225
Quang <quangm202@gmail.com>
2023-07-23 08:06:18 +0700

    Khởi tạo RepositoriesInterface và  BaseRepositories

--- app/Repositories/RepositoriesInterface.php
+++ app/Repositories/RepositoriesInterface.php
@@ -4,5 +4,9 @@ namespace App\Repositories;
 
 interface RepositoriesInterface
 {
-
+    public function all();
+    public function find(int $id);
+    public function create(array $data);
+    public function update(array $data, $id);
+    public function delete(int $id);
 }
