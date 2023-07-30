4736be2d7dd0b6134184a48b848ec6830b37c06c
Quang <quangm202@gmail.com>
2023-07-30 18:21:23 +0700

    Update oders table

--- database/migrations/2020_12_09_085528_create_orders_table.php
+++ database/migrations/2020_12_09_085528_create_orders_table.php
@@ -16,6 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
 
+            $table->integer('user_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('company_name')->nullable();
@@ -26,6 +27,7 @@ class CreateOrdersTable extends Migration
             $table->string('email');
             $table->string('phone');
             $table->string('payment_type');
+            $table->integer('status');
 
             $table->timestamps();
         });
