04131b97d11fa6a7d4cac19586f98a9643070faa
Quang <quangm202@gmail.com>
2023-07-31 12:23:01 +0700

    Minor update

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -23,7 +23,7 @@
 
         <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
             <li class="nav-item">
-                <a href="./user-edit.html" class="nav-link">
+                <a href="/admin/user/{{$user->id}}/edit" class="nav-link">
                     <span class="btn-icon-wrapper pr-2 opacity-8">
                         <i class="fa fa-edit fa-w-20"></i>
                     </span>
