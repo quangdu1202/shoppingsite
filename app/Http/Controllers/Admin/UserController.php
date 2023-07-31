aa6a5bf1aba2907c65a4882278ea3e1781189589
Quang <quangm202@gmail.com>
2023-07-31 12:23:54 +0700

    Hoàn thiện edit user info

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -88,7 +88,7 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return view('admin.user.edit', compact('user'));
     }
 
     /**
@@ -100,7 +100,28 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $data = $request->all();
+        if ($request->get('password') != null){
+            if ($request->get('password') != $request->get('password_confirmation')) {
+                return back()->with('notification', 'Confirmation password does not match!');
+            }
+            $data['password'] = Hash::make($request->get('password'));
+        }else {
+            unset($data['password']);
+        }
+
+        if ($request->hasFile('image')) {
+            $data['avatar'] = Common::uploadFile($request->file('image'), 'front/img/user/');
+
+            $file_name_old = $request->get('image_old');
+            if ($file_name_old != '') {
+                unlink('front/img/user/' .  $file_name_old);
+            }
+        }
+
+        $this->userService->update($data, $user->id);
+
+        return redirect('admin/user/' . $user->id);
     }
 
     /**
