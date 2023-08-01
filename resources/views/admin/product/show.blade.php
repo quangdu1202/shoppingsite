ccf9d67a543ac43c101a2f3933b47070295cdd21
Quang <quangm202@gmail.com>
2023-08-01 08:22:29 +0700

    Hoàn thiện hiển thị thông tin chi tiết sản phẩm

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -28,125 +28,135 @@
                     <div class="card-body display_data">
 
                         <div class="position-relative row form-group">
-                            <label for="" class="col-md-3 text-md-right col-form-label">Images</label>
-                            <div class="col-md-9 col-xl-8">
+                            <label for="" class="col-md-4 text-md-right col-form-label">Images</label>
+                            <div class="col-md-8 col-xl-7">
                                 <ul class="text-nowrap overflow-auto" id="images">
+                                    @foreach($product->productImages as $productImage)
                                     <li class="d-inline-block mr-1" style="position: relative;">
-                                        <img style="height: 150px;" src="assets/images/_default-product.jpg"
+                                        <img style="height: 150px;" src="./front/img/products/{{$productImage->path}}"
                                             alt="Image">
                                     </li>
+                                    @endforeach
                                 </ul>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="brand_id"
-                                class="col-md-3 text-md-right col-form-label">Product Images</label>
-                            <div class="col-md-9 col-xl-8">
+                                class="col-md-4 text-md-right col-form-label">Product Images</label>
+                            <div class="col-md-8 col-xl-7">
                                 <p><a href="./product-image.html">Manage images</a></p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="brand_id"
-                                class="col-md-3 text-md-right col-form-label">Product Details</label>
-                            <div class="col-md-9 col-xl-8">
+                                class="col-md-4 text-md-right col-form-label">Product Details</label>
+                            <div class="col-md-8 col-xl-7">
                                 <p><a href="./product-detail.html">Manage details</a></p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="brand_id"
-                                class="col-md-3 text-md-right col-form-label">Brand</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>Calvin Klein</p>
+                                class="col-md-4 text-md-right col-form-label">Brand</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->brand->name}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="product_category_id"
-                                class="col-md-3 text-md-right col-form-label">Category</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>Men</p>
+                                class="col-md-4 text-md-right col-form-label">Category</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->ProductCategory->name}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
-                            <label for="name" class="col-md-3 text-md-right col-form-label">Name</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>Pure Pineapple</p>
+                            <label for="name" class="col-md-4 text-md-right col-form-label">Name</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->name}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="content"
-                                class="col-md-3 text-md-right col-form-label">Content</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>High quality fabric, modern and youthful design</p>
+                                class="col-md-4 text-md-right col-form-label">Content</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->content}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="price"
-                                class="col-md-3 text-md-right col-form-label">Price</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>$629.99</p>
+                                class="col-md-4 text-md-right col-form-label">Price</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>${{$product->price}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="discount"
-                                class="col-md-3 text-md-right col-form-label">Discount</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>$495.00</p>
+                                class="col-md-4 text-md-right col-form-label">Discount</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>${{$product->discount}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="qty"
-                                class="col-md-3 text-md-right col-form-label">Qty</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>20</p>
+                                class="col-md-4 text-md-right col-form-label">Qty</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->qty}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="weight"
-                                class="col-md-3 text-md-right col-form-label">Weight</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>1.3</p>
+                                class="col-md-4 text-md-right col-form-label">Weight</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->weight}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="sku"
-                                class="col-md-3 text-md-right col-form-label">SKU</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>00012</p>
+                                class="col-md-4 text-md-right col-form-label">SKU</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->sku}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="tag"
-                                class="col-md-3 text-md-right col-form-label">Tag</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>Clothing</p>
+                                class="col-md-4 text-md-right col-form-label">Tag</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->tag}}</p>
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="featured"
-                                class="col-md-3 text-md-right col-form-label">Featured</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>Yes</p>
+                                class="col-md-4 text-md-right col-form-label">Featured</label>
+                            <div class="col-md-8 col-xl-7">
+                                @if($product->featured)
+                                    <div class="badge badge-success mt-2">
+                                        Yes
+                                    </div>
+                                @else
+                                    <div class="badge badge-danger mt-2">
+                                        No
+                                    </div>
+                                @endif
                             </div>
                         </div>
 
                         <div class="position-relative row form-group">
                             <label for="description"
-                                class="col-md-3 text-md-right col-form-label">Description</label>
-                            <div class="col-md-9 col-xl-8">
-                                <p>Product description</p>
+                                class="col-md-4 text-md-right col-form-label">Description</label>
+                            <div class="col-md-8 col-xl-7">
+                                <p>{{$product->description}}</p>
                             </div>
                         </div>
                     </div>
